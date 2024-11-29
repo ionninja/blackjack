15264a46654915148f2c58931910bd763a3b8f20
ionninja <130919389+ionninja@users.noreply.github.com>
2024-11-29 13:55:30 +0000

    graph desc

--- src/schema.ts
+++ src/schema.ts
@@ -2,40 +2,183 @@ import { gql } from 'graphql-tag';
 import type { Query, Mutation, Table, GameHistory, User, Hand } from './types';
 
 export const typeDefs = gql`
+  """
+  Represents a user in the system.
+  """
   type User {
+    """
+    The unique identifier of the user.
+    """
     userId: ID!
+    """
+    The username of the user.
+    """
     username: String!
+    """
+    The password of the user.
+    """
+    password: String!
   }
 
+  """
+  Represents a table in the game.
+  """
   type Table {
+    """
+    The unique identifier of the table.
+    """
     tableId: ID!
+    """
+    The maximum number of seats at the table.
+    """
     maxSeats: Int!
+    """
+    The list of user IDs occupying the seats at the table.
+    """
     seats: [ID!]!
   }
 
+  """
+  Represents the game history of a user.
+  """
   type GameHistory {
+    """
+    The name of the game.
+    """
     game: String!
+    """
+    The result of the game.
+    """
     result: String!
   }
 
+  """
+  Represents a hand of cards.
+  """
   type Hand {
+    """
+    The list of cards in the hand.
+    """
     cards: [String!]!
+    """
+    The value of the hand.
+    """
     value: Int!
   }
 
+  """
+  The root query type.
+  """
   type Query {
+    """
+    Retrieves the list of tables.
+    """
     getTables: [Table!]!
+    """
+    Retrieves the game history of a user by their ID.
+    """
     getGameHistory(userId: ID!): [GameHistory!]!
   }
 
+  """
+  The root mutation type.
+  """
   type Mutation {
-    registerUser(username: String!, password: String!): User!
-    authenticateUser(username: String!, password: String!): User!
-    createTable(tableId: ID!, maxSeats: Int!): Table!
-    joinTable(tableId: ID!, userId: ID!): Table!
-    addGameHistory(userId: ID!, game: String!, result: String!): GameHistory!
-    startBlackjackGame(tableId: ID!): [Hand!]!
-    playerAction(tableId: ID!, userId: ID!, action: String!): Hand!
+    """
+    Registers a new user with the provided username and password.
+    """
+    registerUser(
+      """
+      The username of the new user.
+      """
+      username: String!,
+      """
+      The password of the new user.
+      """
+      password: String!
+    ): User!
+    """
+    Authenticates a user with the provided username and password.
+    """
+    authenticateUser(
+      """
+      The username of the user.
+      """
+      username: String!,
+      """
+      The password of the user.
+      """
+      password: String!
+    ): User!
+    """
+    Creates a new table with the provided table ID and maximum number of seats.
+    """
+    createTable(
+      """
+      The unique identifier of the table.
+      """
+      tableId: ID!,
+      """
+      The maximum number of seats at the table.
+      """
+      maxSeats: Int!
+    ): Table!
+    """
+    Allows a user to join a table with the provided table ID and user ID.
+    """
+    joinTable(
+      """
+      The unique identifier of the table.
+      """
+      tableId: ID!,
+      """
+      The unique identifier of the user.
+      """
+      userId: ID!
+    ): Table!
+    """
+    Adds a game history entry for a user with the provided user ID, game name, and result.
+    """
+    addGameHistory(
+      """
+      The unique identifier of the user.
+      """
+      userId: ID!,
+      """
+      The name of the game.
+      """
+      game: String!,
+      """
+      The result of the game.
+      """
+      result: String!
+    ): GameHistory!
+    """
+    Starts a new blackjack game at the specified table.
+    """
+    startBlackjackGame(
+      """
+      The unique identifier of the table.
+      """
+      tableId: ID!
+    ): [Hand!]!
+    """
+    Performs an action (hit, stand, double) for a player in a blackjack game.
+    """
+    playerAction(
+      """
+      The unique identifier of the table.
+      """
+      tableId: ID!,
+      """
+      The unique identifier of the user.
+      """
+      userId: ID!,
+      """
+      The action to perform (hit, stand, double).
+      """
+      action: String!
+    ): Hand!
   }
 `;
 
